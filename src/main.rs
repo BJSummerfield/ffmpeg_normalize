0b09f95a58616cae0e7622dd0c13cb7dc1c5e954
BJSummerfield <brianjsummerfield@gmail.com>
2024-04-26 16:32:01 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use clap::{builder::Command, Arg, ArgAction, ArgMatches};
 use core::time::Duration;
 use serde::{Deserialize, Serialize};
-use std::io;
-use std::process::{Command as ProcessCommand, Stdio};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-use std::thread;
+use std::{
+    io::{self, IsTerminal},
+    process::{Command as ProcessCommand, Stdio},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread,
+};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Loudness {
@@ -24,33 +28,12 @@ struct CliConfig {
     down_mix: bool,
 }
 
-struct ProgressSpinner;
-
-impl ProgressSpinner {
-    fn show_progress() -> (Arc<AtomicBool>, thread::JoinHandle<()>) {
-        const PROGRESS_CHARS: [&str; 12] =
-            ["⠂", "⠃", "⠁", "⠉", "⠈", "⠘", "⠐", "⠰", "⠠", "⠤", "⠄", "⠆"];
-        let finished = Arc::new(AtomicBool::new(false));
-        let stop_signal = Arc::clone(&finished);
-        let handle = thread::spawn(move || {
-            for pc in PROGRESS_CHARS.iter().cycle() {
-                if stop_signal.load(Ordering::Relaxed) {
-                    break;
-                };
-                eprint!("Processing 1st Loudnorm Pass {}\r", pc);
-                thread::sleep(Duration::from_millis(250));
-            }
-        });
-        (finished, handle)
-    }
-}
-
 impl CliConfig {
-    fn from_matches(matches: &ArgMatches) -> Result<Self, &'static str> {
+    fn new(matches: &ArgMatches) -> Result<Self, io::Error> {
         Ok(Self {
             input_path: matches
                 .get_one::<String>("input")
-                .ok_or("Missing input file path")?
+                .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Missing input file path"))?
                 .clone(),
             integrated_loudness: matches
                 .get_one::<String>("integrated_loudness")
@@ -109,23 +92,18 @@ impl LoudnessAnalyzer {
         let filter_settings = FilterSettings::construct(config, None);
         let output = Self::analyze_loudness(&config.input_path, &filter_settings)?;
 
-        match serde_json::from_str::<Loudness>(&Self::extract_json(&output)) {
-            Ok(loudness) => {
-                println!("{}", FilterSettings::construct(config, Some(&loudness)));
-                Ok(())
-            }
-            Err(e) => {
-                eprintln!("Failed to parse JSON: {}", e);
-                Err(io::Error::new(
+        serde_json::from_str::<Loudness>(&Self::extract_json(&output))
+            .map(|loudness| println!("{}", FilterSettings::construct(config, Some(&loudness))))
+            .map_err(|e| {
+                io::Error::new(
                     io::ErrorKind::InvalidData,
-                    "Invalid JSON data",
-                ))
-            }
-        }
+                    format!("Failed to parse JSON: {}", e),
+                )
+            })
     }
 
     fn analyze_loudness(input_path: &str, filter_settings: &str) -> io::Result<String> {
-        let (finished, spinner_handle) = ProgressSpinner::show_progress();
+        let spinner = ProgressSpinner::show_progress();
 
         let process = ProcessCommand::new("ffmpeg")
             .args(&[
@@ -143,14 +121,8 @@ impl LoudnessAnalyzer {
             .spawn()?;
 
         let output = process.wait_with_output()?;
+        spinner.store(false, Ordering::SeqCst);
 
-        finished.store(true, Ordering::Relaxed);
-
-        if let Err(e) = spinner_handle.join() {
-            eprintln!("Error stopping the spinner: {:?}", e);
-        }
-
-        // Check if FFmpeg was successful
         if output.status.success() {
             Ok(String::from_utf8_lossy(&output.stderr).to_string())
         } else {
@@ -163,9 +135,10 @@ impl LoudnessAnalyzer {
 
     fn extract_json(output: &str) -> String {
         let json_start = output.rfind('{').unwrap_or(0);
-        let json_end = output[json_start..].find('}').unwrap_or(output.len() - 1) + json_start + 1;
-        let json = &output[json_start..json_end];
-        json.to_string()
+        output[json_start..].find('}').map_or_else(
+            || String::new(),
+            |end| output[json_start..=json_start + end].to_string(),
+        )
     }
 }
 
@@ -173,33 +146,51 @@ struct FilterSettings;
 
 impl FilterSettings {
     fn construct(config: &CliConfig, loudness: Option<&Loudness>) -> String {
-        let mut filter = format!(
-            "{}loudnorm=I={}:LRA={}:TP={}",
-            if config.down_mix {
-                "aformat=sample_fmts=s16:sample_rates=48000:channel_layouts=stereo,"
-            } else {
-                ""
-            },
+        let base = if config.down_mix {
+            "aformat=sample_fmts=s16:sample_rates=48000:channel_layouts=stereo,"
+        } else {
+            ""
+        };
+        let loudness_params = loudness.map_or_else(
+            || ":print_format=json".to_string(),
+            |l| format!(":linear=true:measured_I={}:measured_TP={}:measured_LRA={}:measured_thresh={}:offset={}",
+                        l.input_i, l.input_tp, l.input_lra, l.input_thresh, l.target_offset));
+        format!(
+            "{}loudnorm=I={}:LRA={}:TP={}{}",
+            base,
             config.integrated_loudness,
             config.loudness_range,
             config.true_peak,
-        );
+            loudness_params
+        )
+    }
+}
 
-        if let Some(l) = loudness {
-            filter += &format!(
-                ":linear=true:measured_I={}:measured_TP={}:measured_LRA={}:measured_thresh={}:offset={}",
-                l.input_i, l.input_tp, l.input_lra, l.input_thresh, l.target_offset
-            );
-        } else {
-            filter += ":print_format=json";
+struct ProgressSpinner;
+
+impl ProgressSpinner {
+    fn show_progress() -> Arc<AtomicBool> {
+        const PROGRESS_CHARS: [&str; 12] =
+            ["⠂", "⠃", "⠁", "⠉", "⠈", "⠘", "⠐", "⠰", "⠠", "⠤", "⠄", "⠆"];
+        let finished = Arc::new(AtomicBool::new(false));
+        if io::stderr().is_terminal() {
+            let stop_signal = Arc::clone(&finished);
+            let _ = thread::spawn(move || {
+                for pc in PROGRESS_CHARS.iter().cycle() {
+                    if stop_signal.load(Ordering::Relaxed) {
+                        break;
+                    };
+                    eprint!("Processing {}\r", pc);
+                    thread::sleep(Duration::from_millis(250));
+                }
+            });
         }
-
-        filter
+        finished
     }
 }
 
 fn main() -> io::Result<()> {
     let matches = CliConfig::setup_cli();
-    let config = CliConfig::from_matches(&matches).expect("Error parsing command line arguments");
+    let config = CliConfig::new(&matches).expect("Error parsing command line arguments");
     LoudnessAnalyzer::analyze_and_print_loudness(&config)
 }
